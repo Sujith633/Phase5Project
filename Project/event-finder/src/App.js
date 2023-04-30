edc64b167e8bfa0be716ae5ee90d692fb79ec979
Sujith633 <sujithreddy633@gmail.com>
2023-04-30 19:50:02 +0530

    Event Code

--- Project/event-finder/src/App.js
+++ Project/event-finder/src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import Header from './components/Header';
+import About from './components/About';
+import Events from './components/Events';
+// import Posts from './components/Posts';
+import TextForm from './components/TextForm';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+      <Header title={"EventFinder"}/>
+        <div className='container my-3'>
+          <Routes>
+            <Route path="/about" element={<About heading='About Us'/>} />
+            {/* <Route path="/" element={<Posts/>} /> */}
+            <Route path="/" element={<TextForm heading="Enter the text here to Analyze here"/>} />
+            <Route path="/events" element={<Events/>} />
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
